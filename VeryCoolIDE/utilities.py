33835188389f9a7ab37d71b553801e0afd7f2464
billyeatcookies <billydevbusiness@gmail.com>
2021-05-29 13:44:03 +0200

    add checks for python and pyinstaller package

--- VeryCoolIDE/utilities.py
+++ VeryCoolIDE/utilities.py
@@ -1,3 +1,8 @@
+import sys, os
+import subprocess
+import asyncio
+
+
 def center(win, width=100, height=100):
     win.update_idletasks()
 
@@ -14,3 +19,24 @@ def center(win, width=100, height=100):
     win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
     
     win.deiconify()
+
+def fetch_installed_packages():
+    req = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in req.split()]
+    return installed_packages
+
+
+def check_python_installation():
+    path = os.environ['PATH'].split(';')
+    for i in path:
+        if 'Python3' in i:
+            return True
+    return False
+
+
+def check_pyinstaller_installation():
+    packages = fetch_installed_packages()
+    if 'pyinstaller' in packages:
+        return True
+
+    return False
